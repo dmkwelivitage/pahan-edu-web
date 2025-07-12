f33233f87ee80c095b7583067f3462547cb55070
dmkwelivitage <dmkwelivitage@gmail.com>
2025-07-12 16:26:08 +0530

    feat: add DBConnection class and configure MySQL with JUnit support

---
+++ src/main/java/com/uni/util/DBConnection.java
@@ -0,0 +1,22 @@
+package com.uni.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static final String URL = "jdbc:mysql://localhost:3306/pahan-edu";
+    public static final String USER = "root";
+    public static final String PASSWORD = "";
+
+    private static Connection connection;
+
+    private DBConnection() {}
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return connection;
+    }
+}
