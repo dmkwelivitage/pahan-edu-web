17e4101e513d9e08e336d56c7d79f8888fe7a5a2
dmkwelivitage <dmkwelivitage@gmail.com>
2025-07-16 01:55:23 +0530

    chore: Updated DBConnection - Fixed Driver Error and Property Loading

--- src/main/java/com/uni/util/DBConnection.java
+++ src/main/java/com/uni/util/DBConnection.java
@@ -10,25 +10,31 @@ public class DBConnection {
 
     private static Connection connection;
 
-    private DBConnection() {}
+    private DBConnection() {
+    }
 
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
-//            try(InputStream input = DBConnection.class.getClassLoader()
-//                    .getResourceAsStream("application.properties")) {
-//                Properties prop = new Properties();
-//                prop.load(input);
-//
-//                String url = prop.getProperty("db.url");
-//                String username = prop.getProperty("db.username");
-//                String password = prop.getProperty("db.password");
+            try {
+                // Load the MySQL JDBC driver
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("MySQL JDBC Driver not found.", e);
+            }
+            try(InputStream input = DBConnection.class.getClassLoader()
+                    .getResourceAsStream("application.properties")) {
+                Properties prop = new Properties();
+                prop.load(input);
 
-//                connection = DriverManager.getConnection(url, username, password);
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/pahan_edu", "root", "");
-//            } catch (Exception ex) {
-//                throw new SQLException("Unable to load DB configuration:" , ex.getMessage(), ex);
-//            }
+                String url = prop.getProperty("db.url");
+                String username = prop.getProperty("db.username");
+                String password = prop.getProperty("db.password");
+
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (Exception ex) {
+                throw new SQLException("Unable to load DB configuration:" , ex.getMessage(), ex);
+            }
+            }
+            return connection;
         }
-        return connection;
     }
-}
